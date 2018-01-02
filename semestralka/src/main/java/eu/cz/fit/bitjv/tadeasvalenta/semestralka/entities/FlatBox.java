8eeed7a3193ede0d8776cc0bce36219f0296b13b
Tadeáš Valenta <valentad@fit.cvut.cz>
2018-01-02 16:39:49 +0100

    add boxes

---
+++ semestralka/src/main/java/eu/cz/fit/bitjv/tadeasvalenta/semestralka/entities/FlatBox.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package eu.cz.fit.bitjv.tadeasvalenta.semestralka.entities;
+
+import java.util.List;
+
+/**
+ *
+ * @author tadas
+ */
+public class FlatBox {
+     private List<Flat> flats;
+
+    public List<Flat> getArticles() {
+        return flats;
+    }
+
+    public void setArticles(List<Flat> flats) {
+        this.flats = flats;
+    }
+}
---
+++ semestralka/src/main/java/eu/cz/fit/bitjv/tadeasvalenta/semestralka/entities/OccupierBox.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package eu.cz.fit.bitjv.tadeasvalenta.semestralka.entities;
+
+import java.util.List;
+
+/**
+ *
+ * @author tadas
+ */
+public class OccupierBox {
+     private List<Occupier> occupiers;
+
+    public List<Occupier> getArticles() {
+        return occupiers;
+    }
+
+    public void setArticles(List<Occupier> occupiers) {
+        this.occupiers = occupiers;
+    }
+}
