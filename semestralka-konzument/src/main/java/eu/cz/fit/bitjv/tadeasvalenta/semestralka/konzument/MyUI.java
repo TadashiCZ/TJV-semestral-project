32e04bde1fe43fd06f039c369283cd2b263aabfa
Tadeáš Valenta <valentad@fit.cvut.cz>
2018-01-04 12:41:04 +0100

    front-end CRUD working

--- semestralka-konzument/src/main/java/eu/cz/fit/bitjv/tadeasvalenta/semestralka/konzument/MyUI.java
+++ semestralka-konzument/src/main/java/eu/cz/fit/bitjv/tadeasvalenta/semestralka/konzument/MyUI.java
@@ -7,6 +7,7 @@ import com.vaadin.annotations.VaadinServletConfiguration;
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.server.VaadinServlet;
 import com.vaadin.ui.Button;
+import com.vaadin.ui.ComboBox;
 import com.vaadin.ui.FormLayout;
 import com.vaadin.ui.Grid;
 import com.vaadin.ui.HorizontalLayout;
@@ -26,6 +27,8 @@ import eu.cz.fit.bitjv.tadeasvalenta.semestralka.konzument.dto.PlugType;
 import eu.cz.fit.bitjv.tadeasvalenta.semestralka.rest.FlatClient;
 import eu.cz.fit.bitjv.tadeasvalenta.semestralka.rest.OccupierClient;
 import eu.cz.fit.bitjv.tadeasvalenta.semestralka.rest.PlugClient;
+import java.util.ArrayList;
+import java.util.EnumSet;
 import java.util.List;
 
 /**
@@ -39,11 +42,15 @@ import java.util.List;
 public class MyUI extends UI {
     private Label occupier = new Label();
     private TextField occupiersCount = new TextField("Flat Count");
+    private Grid<Flat> flatGrid = new Grid<>();
+    private Grid<Occupier> occupierGrid = new Grid<>();        
+    private Grid<Plug> plugGrid = new Grid<>();
+    
     
     @Override
     protected void init(VaadinRequest vaadinRequest) {
         final VerticalLayout layout = new VerticalLayout();
-        HorizontalLayout flatBox = new HorizontalLayout();
+        HorizontalLayout formBox = new HorizontalLayout();
         
          final TextField name = new TextField();
         name.setCaption("Input numer of drivers:");
@@ -76,20 +83,15 @@ public class MyUI extends UI {
         }
         
        
-        initData(os, fs, ps);
+       // initData(os, fs, ps);
         
-        Grid<Occupier> occupierGrid = new Grid<>();
         occupierGrid = initOccupierGrid(os.findAllOccupiers_JSON(OccupierBox.class).getOccupiers());
-        
-        Grid<Flat> flatGrid = new Grid<>();
         flatGrid = initFlatGrid(fs.findAllFlats_JSON(FlatBox.class).getFlats());
-        flatBox.addComponent(flatGrid);
-        
-        Grid<Plug> plugGrid = new Grid<>();
         plugGrid = initPlugGrid(ps.findAllPlugs_JSON(PlugBox.class).getPlugs());
-        /*
+               
+        
         FormLayout flatForm = new FormLayout();
-        flatBox.addComponent(flatForm);
+       
         TextField ff1 = new TextField("ID");
         //ff1.setEnabled(false);
         TextField ff2 = new TextField("Area");
@@ -98,7 +100,6 @@ public class MyUI extends UI {
         Button fb1 = new Button("Edit");
         Button fb2 = new Button("Delete");
         Button fb3 = new Button("Create");
-        
         fb1.addClickListener(e -> {
             Flat flat = new Flat();
             flat.setId(Long.parseLong(ff1.getValue()));
@@ -109,15 +110,127 @@ public class MyUI extends UI {
             FlatBox tmp = fs.findAllFlats_JSON(FlatBox.class);
             flatGrid.setItems(tmp.getFlats());
             flatForm.setVisible(false);
-           
+        });
+        fb2.addClickListener(e -> {
+            fs.remove(ff1.getValue());
+            FlatBox tmp = fs.findAllFlats_JSON(FlatBox.class);
+            if (!tmp.getFlats().isEmpty()) flatGrid.setHeightByRows(tmp.getFlats().size());
+            flatGrid.setItems(tmp.getFlats());
+            flatForm.setVisible(false);
+         });
+        fb3.addClickListener(e -> {
+            Flat flat = new Flat();
+            flat.setId(Long.parseLong(ff1.getValue()));
+            flat.setArea(Long.parseLong(ff2.getValue()));
+            flat.setRent(Long.parseLong(ff3.getValue()));
+            flat.setDoorNumber(Long.parseLong(ff4.getValue()));
+            fs.create_JSON(flat);
+            FlatBox tmp = fs.findAllFlats_JSON(FlatBox.class);
+            flatGrid.setHeightByRows(tmp.getFlats().size());
+            flatGrid.setItems(tmp.getFlats());
+            flatForm.setVisible(false);
         });
         flatForm.addComponents(ff1, ff2, ff3, ff4, new HorizontalLayout(fb1, fb2, fb3));
+                
+        
+        FormLayout occupierForm = new FormLayout();
+        
+        TextField of1 = new TextField("ID");
+        //of1.setEnabled(false);
+        TextField of2 = new TextField("Name");
+        TextField of3 = new TextField("E-mail");
+        TextField of4 = new TextField("Phone");
+        Button ob1 = new Button("Edit");
+        Button ob2 = new Button("Delete");
+        Button ob3 = new Button("Create");
+        ob1.addClickListener(e -> {
+            Occupier occupier = new Occupier();
+            occupier.setId(Long.parseLong(of1.getValue()));
+            occupier.setName(of2.getValue());
+            occupier.setEmail(of3.getValue());
+            occupier.setPhone(of4.getValue());
+            os.edit_JSON(occupier, of1.getValue());
+            OccupierBox tmp = os.findAllOccupiers_JSON(OccupierBox.class);
+            occupierGrid.setItems(tmp.getOccupiers());
+            occupierForm.setVisible(false);
+        });
+        ob2.addClickListener(e -> {
+            os.remove(of1.getValue());
+            OccupierBox tmp = os.findAllOccupiers_JSON(OccupierBox.class);
+            if (!tmp.getOccupiers().isEmpty()) occupierGrid.setHeightByRows(tmp.getOccupiers().size());
+            occupierGrid.setItems(tmp.getOccupiers());
+            occupierForm.setVisible(false);
+        });
+        ob3.addClickListener(e -> {
+            Occupier occupier = new Occupier();
+            occupier.setId(Long.parseLong(of1.getValue()));
+            occupier.setName(of2.getValue());
+            occupier.setEmail(of3.getValue());
+            occupier.setPhone(of4.getValue());
+            os.create_JSON(occupier);
+            OccupierBox tmp = os.findAllOccupiers_JSON(OccupierBox.class);
+            occupierGrid.setHeightByRows(tmp.getOccupiers().size());
+            occupierGrid.setItems(tmp.getOccupiers());
+            occupierForm.setVisible(false);
+        });
+        occupierForm.addComponents(of1,of2,of3,of4, new HorizontalLayout(ob1,ob2,ob3));
+        
+        List<PlugType> plugTypes = new ArrayList<>(EnumSet.allOf(PlugType.class));  
+        FormLayout plugForm = new FormLayout();
+        
+        TextField pf1 = new TextField("ID");
+        //pf1.setEnabled(false);
+        TextField pf2 = new TextField("Provider");
+        TextField pf3 = new TextField("Payment");
+        //TextField pf4 = new TextField("Plug type");
+        ComboBox pf4 = new ComboBox<>("Select plug type");
+        pf4.setEmptySelectionAllowed(false);
+        pf4.setItems(plugTypes);
+        Button pb1 = new Button("Edit");
+        Button pb2 = new Button("Delete");
+        Button pb3 = new Button("Create");
+        pb1.addClickListener(e -> {
+            Plug plug = new Plug();
+            plug.setId(Long.parseLong(pf1.getValue()));
+            plug.setProvider(pf2.getValue());
+            plug.setPayment(Long.parseLong(pf3.getValue()));
+            plug.setType((PlugType) pf4.getValue());
+            ps.edit_JSON(plug, pf1.getValue());
+            PlugBox tmp = ps.findAllPlugs_JSON(PlugBox.class);
+            plugGrid.setItems(tmp.getPlugs());
+            plugForm.setVisible(false);
+        });
+        pb2.addClickListener(e -> {
+            ps.remove(pf1.getValue());
+            PlugBox tmp = ps.findAllPlugs_JSON(PlugBox.class);
+            if (!tmp.getPlugs().isEmpty()) plugGrid.setHeightByRows(tmp.getPlugs().size());
+            plugGrid.setItems(tmp.getPlugs());
+            plugForm.setVisible(false);
+        });
+        pb3.addClickListener(e -> {
+            Plug plug = new Plug();
+            plug.setId(Long.parseLong(pf1.getValue()));
+            plug.setProvider(pf2.getValue());
+            plug.setPayment(Long.parseLong(pf3.getValue()));
+            plug.setType((PlugType) pf4.getValue());
+            ps.create_JSON(plug);
+            PlugBox tmp = ps.findAllPlugs_JSON(PlugBox.class);
+            plugGrid.setItems(tmp.getPlugs());
+            plugGrid.setHeightByRows(tmp.getPlugs().size());
+            plugForm.setVisible(false);
+        });
         
         
-        layout.addComponent(flatForm);*/
+        
+        plugForm.addComponents(pf1, pf2, pf3, pf4, new HorizontalLayout(pb1, pb2, pb3));
+        
+        formBox.addComponent(flatForm);
+        formBox.addComponent(occupierForm);
+        formBox.addComponent(plugForm);
         layout.addComponent(flatGrid);
         layout.addComponent(occupierGrid);
         layout.addComponent(plugGrid);
+        layout.addComponent(formBox);
         setContent(layout);
        
     }
